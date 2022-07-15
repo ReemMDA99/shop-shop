ce5c9a922c3d57650f026e60219845b86cce28b9
Reem Ahmed <reem.mda0909@gmail.com>
2022-07-15 09:27:42 -0600

    updates

---
+++ client/build/precache-manifest.190bafc6c79d7aa487053bd32ea0a93c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9698c13968d1961af3ce1c66c9367b30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "351ad9409e2efc635cd0",
+    "url": "/static/css/main.683cb2f6.chunk.css"
+  },
+  {
+    "revision": "1f7b4fbde98b6242bd87",
+    "url": "/static/js/2.7f18ecf0.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.7f18ecf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "351ad9409e2efc635cd0",
+    "url": "/static/js/main.583a3a60.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.583a3a60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
